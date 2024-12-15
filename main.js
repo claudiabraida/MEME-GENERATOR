6bd9a5ad34280f05a5579802f97efc6d14696d0d
Claudia Braida <braidacreaciones@gmail.com>
2024-12-15 16:39:00 -0300

    :bento: modified: index.html

---
+++ main.js
@@ -0,0 +1,48 @@
+/* ************ UNIVERSAL SELECTOR FUNCTION ************ */
+const $ = element => document.querySelector(element);
+
+/* ************ VARIABLES ************ */
+/* BUTTONS NAV */
+const $buttonImage = $(".buttonImage");
+const $buttonText = $(".buttonText");
+
+/* PANELS CONTROLS */
+const $asideImagePanelControls = $(".imagePanelControls");
+const $asideTextPanelControls = $(".textPanelControls");
+
+/* BUTTONS CLOSE PANELS */
+const $buttonCloseImagePanelControls = $(".closeImagePanelControls");
+const $buttonCloseTextPanelControls = $ (".closeTextPanelControls");
+
+/* *********** EVENTS *********** */
+
+/* -------- BUTTONS NAV -------- */
+$buttonText.addEventListener("click", () => {
+  $asideImagePanelControls.style.display = "flex"
+  if( $asideImagePanelControls.style.display = "flex") {
+  $asideImagePanelControls.style.display = "none"
+  $asideTextPanelControls.style.display = "flex"
+  }
+  
+})
+
+$buttonImage.addEventListener("click", () => {
+  $asideTextPanelControls.style.display = "flex"
+  if ($asideTextPanelControls.style.display = "flex") {
+    $asideTextPanelControls.style.display = "none"
+    $asideImagePanelControls.style.display = "flex"
+  }
+  
+})
+
+/*BUTTONS CLOSE PANELS*/
+$buttonCloseImagePanelControls.addEventListener("click", () => {
+  $asideImagePanelControls.style.display = "none"
+  // $asideImagePanelControls.style.width = "300px"
+
+})
+
+$buttonCloseTextPanelControls.addEventListener("click", () => {
+  $asideTextPanelControls.style.display = "none"
+})
+
