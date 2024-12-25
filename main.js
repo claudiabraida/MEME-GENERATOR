f1b2975e9e2a0034b8220ecd3c68f8be4be72e14
Claudia Braida <braidacreaciones@gmail.com>
2024-12-24 23:54:07 -0300

    :art: modified : desktop.ini, index.html, main.js, 1-principal.css, 2-header.css, 3-main.css, 4-aside-image.css, 5-aside-text.css, 6-footer.css

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-/* ************ UNIVERSAL SELECTOR FUNCTION ************ */
+/* ✨✨✨✨✨ UNIVERSAL SELECTOR FUNCTION ✨✨✨✨✨ */
 const $ = element => document.querySelector(element);
 
-/* ************ VARIABLES ************ */
-/* BUTTONS NAV */
-const $buttonImage = $(".buttonImage");
-const $buttonText = $(".buttonText");
+/* ✨✨✨✨✨✨✨ VARIABLES ✨✨✨✨✨✨✨ */
 
-/* PANELS CONTROLS */
-const $asideImagePanelControls = $(".imagePanelControls");
-const $asideTextPanelControls = $(".textPanelControls");
+/* ________________ HEADER BUTTONS NAV ________________ */
+const $buttonImage = $(".button-image");
+const $buttonText = $(".button-text");
 
-/* BUTTONS CLOSE PANELS */
-const $buttonCloseImagePanelControls = $(".closeImagePanelControls");
-const $buttonCloseTextPanelControls = $ (".closeTextPanelControls");
+/* _________________ MAIN IMAGE-MEME / URL _________________ */
+const $divImageMeme = $(".image-meme")
+const $inputUrlImageMeme = $("#url-image-meme");
 
-/* *********** EVENTS *********** */
+/* __________________ PANELS CONTROLS __________________ */
+const $asideImagePanelControls = $(".image-panel-controls");
+const $asideTextPanelControls = $(".text-panel-controls");
 
-/* -------- BUTTONS NAV -------- */
+/* ___________ BUTTONS CLOSE PANELS CONTROLS ___________ */
+const $buttonCloseImagePanelControls = $(".close-image-panel-controls");
+const $buttonCloseTextPanelControls = $ (".close-text-panel-controls");
+
+
+/* ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨ */ 
+
+
+/* ✨✨✨✨✨✨✨✨ EVENTS ✨✨✨✨✨✨✨✨ */ 
+
+/* ................ EVENT BUTTONS NAV ................ */
 $buttonText.addEventListener("click", () => {
   $asideImagePanelControls.style.display = "flex"
-  if( $asideImagePanelControls.style.display = "flex") {
-  $asideImagePanelControls.style.display = "none"
-  $asideTextPanelControls.style.display = "flex"
+  if ( $asideImagePanelControls.style.display = "flex") {
+    $asideImagePanelControls.style.display = "none"
+    $asideTextPanelControls.style.display = "flex"
   }
   
 })
@@ -32,10 +41,11 @@ $buttonImage.addEventListener("click", () => {
     $asideTextPanelControls.style.display = "none"
     $asideImagePanelControls.style.display = "flex"
   }
-  
+
 })
 
-/*BUTTONS CLOSE PANELS*/
+
+/* ........... EVENT BUTTONS CLOSE PANELS ........... */
 $buttonCloseImagePanelControls.addEventListener("click", () => {
   $asideImagePanelControls.style.display = "none"
 })
@@ -43,4 +53,49 @@ $buttonCloseImagePanelControls.addEventListener("click", () => {
 $buttonCloseTextPanelControls.addEventListener("click", () => {
   $asideTextPanelControls.style.display = "none"
 })
+
+/* ................... EVENT IMAGE PANEL .................... */
+
+/* ----------------------- EVENT IMAGE URL ----------------------- */
+$inputUrlImageMeme.addEventListener("input", () => {
+  $divImageMeme.style.backgroundImage = `url(${$inputUrlImageMeme.value})`
+})
+
+/* .................... EVENT TEXT PANEL .................... */
+
+/* ------------------- VARIABLES TOP TEXT ------------------- */
+const $sectionContainerMeme = $("container-meme");
+const $pTopText = $(".top-text");
+const $textareaTopText = $("#top-text");
+const $inputCheckboxNoTopText = $("#no-top-text");
+
+/* -------------------- WRITE TOP TEXT -------------------- */
+$textareaTopText.addEventListener("input", () => {
+  $pTopText.innerText = $textareaTopText.value
+})
+
+/* --------------------- NO TOP TEXT --------------------- */
+$inputCheckboxNoTopText.addEventListener("input", () => {
+  $pTopText.innertHtml = $inputCheckboxNoTopText.value
+$pTopText.remove()
+})
+
+/* ---------------- VARIABLES BOTTOM TEXT ---------------- */
+const $pBottomText = $(".bottom-text");
+const $textareaBottomText = $("#bottom-text");
+const $inputCheckboxNoBottomText = $("#no-bottom-text");
+
+/* ----------------- WRITE BOTTOM TEXT ----------------- */
+$textareaBottomText.addEventListener("input", () => {
+  $pBottomText.innerText = $textareaBottomText.value
+
+})
+
+/* ------------------ NO BOTTOM TEXT ------------------ */
+$inputCheckboxNoBottomText.addEventListener("input", () => {
+  $pBottomText.remove()
+})  
+   
+
+/* ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨ */ 
 
