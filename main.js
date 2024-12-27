07f2317fd79b29298267787965c3c6f5f80fd8cf
Claudia Braida <braidacreaciones@gmail.com>
2024-12-27 05:42:29 -0300

    :bulb::alembic: changes: class names, id in html

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
+
 /* ✨✨✨✨✨ UNIVERSAL SELECTOR FUNCTION ✨✨✨✨✨ */
 const $ = element => document.querySelector(element);
 
-/* ____________________________ VARIABLES ____________________________ */
+/* ============================ VARIABLES ============================ */
 
 /*💠💠💠💠💠💠💠💠 VARIABLES HEADER BUTTONS NAV 💠💠💠💠💠💠💠 */
 const $buttonImage = $(".button-image");
 const $buttonText = $(".button-text");
 
+/* 💠💠💠💠💠 VARIABLES BUTTONS CLOSE PANELS CONTROLS 💠💠💠💠💠 */
+const $buttonCloseImagePanelControls = $(".close-image-panel-controls");
+const $buttonCloseTextPanelControls = $ (".close-text-panel-controls");
 
-/* 💠💠💠💠💠💠💠 VARIABLES MAIN IMAGE-MEME / URL 💠💠💠💠💠💠💠*/
+/* 💠💠💠💠💠💠💠💠 VARIABLES PANELS CONTROLS 💠💠💠💠💠💠💠💠 */
+const $asideImagePanelControls = $(".image-panel-controls");
+const $asideTextPanelControls = $(".text-panel-controls");
+
+
+/* -------------------------- IMAGE PANEL  -------------------------- */
+
+/* 💠💠💠💠💠 VARIABLES MAIN: IMAGE-MEME / URL / SIZE 💠💠💠💠💠 */
 const $divImageMeme = $(".image-meme");
 const $inputUrlImageMeme = $("#url-image-meme");
-const $inputImageSize = $("#image-size")
+const $inputRangeImageSize = $("#image-size");
 
+/* 💠💠💠💠 VARIABLES INPUT COLOR BACKGROUND IMAGE (FONDO) 💠💠💠💠 */
+const $inputColorPaletteImage = $("#color-palette-image");
+const $labelNameColorPaletteImage = $(".name-color-palette-image");
 
-/* 💠💠💠💠💠💠💠 VARIABLES COLOR BACKGROUND IMAGE  💠💠💠💠💠💠💠*/
-const $inputColorBackgroundColorImage = $("#background-color-image");
-const $labelBackgroundColorImage = $(".name-background-color-image");
+/* 💠💠💠💠💠💠 VARIABLE SELECT BLEND MODE IMAGE (FONDO) 💠💠💠💠💠💠 */
+const $selectBlendModeImage = $("#select-blend-mode-image")
 
-
-/* 💠💠💠💠 VARIABLES SELECT- OPTIONS COLOR BACKGROUND IMAGE 💠💠💠💠 */
-const $selectBackgroundColor = $("#select-background-color")
-
-const $optionNone = $("#none")  
-const $optionLighten = $("#lighten")  
-const $optionDarken = $("#darken")  
-const $optionDiffernce = $("#difference")
-const $optionLuminosity = $("#luminosity")  
-const $optionMultiply = $("#multiply") 
-
-
-/* 💠💠💠💠💠💠💠💠 VARIABLES FILTERS IMAGE 💠💠💠💠💠💠💠💠 */ 
+/* 💠💠💠💠💠💠💠💠💠 VARIABLES FILTERS IMAGE 💠💠💠💠💠💠💠💠💠 */ 
 const $inputRangeBrightness = $("#brightness");
 const $inputRangeOpacity = $("#opacity");
 const $inputRangeContrast = $("#contrast");
@@ -40,36 +41,26 @@ const $inputRangeSepia = $("#sepia")
 const $inputRangeHue = $("#hue")
 const $inputRangeSaturate = $("#saturate")
 const $inputRangeInvert = $("#invert")
-const $buttonReset = $("#reset");
+const $buttonResetFilterImage = $("#button-reset-filter-image");
 
+/* -------------------------- TEXT PANEL  -------------------------- */
 
-/* 💠💠💠💠💠💠💠💠 VARIABLES PANELS CONTROLS 💠💠💠💠💠💠💠💠 */
-const $asideImagePanelControls = $(".image-panel-controls");
-const $asideTextPanelControls = $(".text-panel-controls");
-
-
-/* 💠💠💠💠💠 VARIABLES BUTTONS CLOSE PANELS CONTROLS 💠💠💠💠💠 */
-const $buttonCloseImagePanelControls = $(".close-image-panel-controls");
-const $buttonCloseTextPanelControls = $ (".close-text-panel-controls");
-
-
-/* 💠💠💠💠💠💠 VARIABLES TOP TEXT  (PANEL TEXT) 💠💠💠💠💠💠 */
-const $sectionContainerMeme = $("container-meme");
+/* 💠💠💠💠💠💠💠💠💠 VARIABLES TOP TEXT 💠💠💠💠💠💠💠💠💠 */
 const $pTopText = $(".top-text");
 const $textareaTopText = $("#top-text");
 const $inputCheckboxNoTopText = $("#no-top-text");
-
 
 /* 💠💠💠💠💠💠 VARIABLES BOTTOM TEXT (PANEL TEXT) 💠💠💠💠💠💠 */
 const $pBottomText = $(".bottom-text");
 const $textareaBottomText = $("#bottom-text");
 const $inputCheckboxNoBottomText = $("#no-bottom-text");
 
-
-/* _____________________________ FUNTIONS _____________________________ */
+/* 💠💠💠💠💠💠💠💠💠 SELECT FONT-FAMILY 💠💠💠💠💠💠💠💠💠 */
+const $selectFontFamily = $("#select-font");
+/* =========================== FUNCTIONS =========================== */
 
 /* 💡💡💡💡💡💡💡💡💡💡💡 FUNTION FILTER FUSION  💡💡💡💡💡💡💡💡💡💡💡 */
-function fusion () {$divImageMeme.style.filter = `
+function fusionFilters () {$divImageMeme.style.filter = `
   brightness(${$inputRangeBrightness.value})
   opacity(${$inputRangeOpacity.value})
   contrast(${$inputRangeContrast.value}%)
@@ -82,12 +73,12 @@ function fusion () {$divImageMeme.style.filter = `
 }
 
 /* 💡💡💡💡💡💡💡💡 FUNTION RESET VALUES FILTER IMAGE 💡💡💡💡💡💡💡💡 */
-function resetValues () {
-  $inputImageSize.min = "0"
-  $inputImageSize.value = "100"
-  $inputImageSize.max = "300"
-  $inputImageSize.step = "1"
-   
+function resetValuesFilters () {
+  $inputRangeImageSize.min = "0"
+  $inputRangeImageSize.value = "100"
+  $inputRangeImageSize.max = "300"
+  $inputRangeImageSize.step = "1"
+  
   /* FILTER BRIGHTNESS */
   $inputRangeBrightness.value = "1"
   $inputRangeBrightness.min = "0"
@@ -105,102 +96,52 @@ function resetValues () {
   $inputRangeContrast.min = "0"
   $inputRangeContrast.max = "400"
   $inputRangeContrast.step = "1"
-   
+  
   /* FILTER BLUR */
   $inputRangeBlur.value = "0"
   $inputRangeBlur.min = "0"
   $inputRangeBlur.max = "10"
   $inputRangeBlur.step = "0.1"
-   
+  
   /* FILTER GRAYSACLE */
   $inputRangeGrayscale.value = "0"
   $inputRangeGrayscale.min = "0"
   $inputRangeGrayscale.max = "100"
   $inputRangeGrayscale.step = "1"
-   
+  
   /* FILTER SEPIA */
   $inputRangeSepia.value = "0"
   $inputRangeSepia.min = "0"
   $inputRangeSepia.max = "100"
   $inputRangeSepia.step = "1"
- 
+  
   /* FILTER HUE */
   $inputRangeHue.value = "0"
   $inputRangeHue.min = "0"
   $inputRangeHue.max = "359"
   $inputRangeHue.step = "1"
- 
+  
   /* FILTER SATURATE */
   $inputRangeSaturate.value = "1"
   $inputRangeSaturate.min = "100"
   $inputRangeSaturate.max = "1000"
   $inputRangeSaturate.step = "10"
- 
+  
   /* FILTER INVERT */
   $inputRangeInvert.value = "0"
   $inputRangeInvert.min = "0"
   $inputRangeInvert.max = "1"
   $inputRangeInvert.step = "0.1"
-
- /* INITIAL VALUES IMAGE-SIZE */
-  $divImageMeme.style.backgroundSize = `${$inputImageSize.value}%`
-  $divImageMeme.style.filter = fusion()
-}
-resetValues()
-
-
-/* 💥💥💥💥💥💥💥💥 EVENT FILTER FUSION IMAGE 💥💥💥💥💥💥💥💥 */
-$inputRangeBrightness.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeOpacity.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeContrast.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
   
-});
+  /* INITIAL VALUES */
+  /* image size */
+  $divImageMeme.style.backgroundSize = `${$inputRangeImageSize.value}%`
+  /* filters*/
+  $divImageMeme.style.filter = fusionFilters()
+}
+resetValuesFilters()
 
-$inputRangeBlur.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeGrayscale.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeSepia.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeHue.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeSaturate.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-$inputRangeInvert.addEventListener("input", () => {
-  $divImageMeme.style.filter = fusion()
-
-});
-
-
-/* _____________________________ EVENTS _____________________________ */
-
-/* 💥💥💥💥💥💥💥 EVENT BUTTON RESET FILTERS IMAGE 💥💥💥💥💥💥💥 */
-$buttonReset.addEventListener("click", resetValues);
+/* ============================== EVENTS ============================== */
 
 /* 💥💥💥💥💥💥💥💥💥💥 EVENT BUTTONS NAV 💥💥💥💥💥💥💥💥💥💥 */
 $buttonText.addEventListener("click", () => {
@@ -209,7 +150,6 @@ $buttonText.addEventListener("click", () => {
     $asideImagePanelControls.style.display = "none"
     $asideTextPanelControls.style.display = "flex"
   }
-  
 })
 
 $buttonImage.addEventListener("click", () => {
@@ -218,25 +158,19 @@ $buttonImage.addEventListener("click", () => {
     $asideTextPanelControls.style.display = "none"
     $asideImagePanelControls.style.display = "flex"
   }
-
 })
 
-
-/* 💥💥💥💥💥💥💥💥💥💥 EVENT IMAGE SIZE 💥💥💥💥💥💥💥💥💥💥 */ 
-$inputImageSize.addEventListener("input", () => {
-  $divImageMeme.style.backgroundSize = `${$inputImageSize.value}%`
-})
 
 /* 💥💥💥💥💥💥 EVENT BUTTONS CLOSE PANELS CONTROLS 💥💥💥💥💥💥 */
 $buttonCloseImagePanelControls.addEventListener("click", () => {
   $asideImagePanelControls.style.display = "none"
 })
 
-
 $buttonCloseTextPanelControls.addEventListener("click", () => {
   $asideTextPanelControls.style.display = "none"
 })
 
+/* ----------------------- EVENT PANEL IMAGE ----------------------- */
 
 /* 💥💥💥💥💥💥💥 EVENT URL IMAGE (PANEL IMAGE) 💥💥💥💥💥💥💥 */
 $inputUrlImageMeme.addEventListener("input", () => {
@@ -244,14 +178,70 @@ $inputUrlImageMeme.addEventListener("input", () => {
 })
 
 
+/* 💥💥💥💥💥💥💥💥💥💥 EVENT IMAGE SIZE 💥💥💥💥💥💥💥💥💥💥 */ 
+$inputRangeImageSize.addEventListener("input", () => {
+  $divImageMeme.style.backgroundSize = `${$inputRangeImageSize.value}%`
+})
 
 
+/* 💥💥💥💥💥 EVENT INPUT COLOR-PALETTE/ LABEL NAME  💥💥💥💥💥 */ 
+$inputColorPaletteImage.addEventListener("input", () => {
+  $labelNameColorPaletteImage.innerText = $inputColorPaletteImage.value
+  $divImageMeme.style.backgroundColor = $inputColorPaletteImage.value
+})
+
+/* 💥💥💥💥💥💥💥💥 EVENT SELECT BLEND MODE 💥💥💥💥💥💥💥💥 */ 
+$selectBlendModeImage.addEventListener("input", () => {
+  $divImageMeme.style.backgroundBlendMode = $selectBlendModeImage.value
+})
+
+/* 💥💥💥 EVENT INPUT RANGE FILTER IMAGE (FUSION FILTERS) 💥💥💥 */
+$inputRangeBrightness.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeOpacity.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeContrast.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeBlur.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeGrayscale.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeSepia.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeHue.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeSaturate.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+$inputRangeInvert.addEventListener("input", () => {
+  $divImageMeme.style.filter = fusionFilters()
+});
+
+/* 💥💥💥💥💥💥 EVENT BUTTON RESET FILTERS IMAGE 💥💥💥💥💥💥 */
+$buttonResetFilterImage.addEventListener("click", resetValuesFilters);
+
+
+/* ----------------------- EVENT PANEL TEXT ----------------------- */
 
 /* 💥💥💥💥💥💥💥💥💥💥 WRITE TOP TEXT 💥💥💥💥💥💥💥💥💥💥 */
 $textareaTopText.addEventListener("input", () => {
   $pTopText.innerText = $textareaTopText.value
 })
-
 
 /* 💥💥💥💥💥💥💥💥💥💥 NO TOP TEXT 💥💥💥💥💥💥💥💥💥💥 */
 $inputCheckboxNoTopText.addEventListener("input", () => {
@@ -259,19 +249,21 @@ $inputCheckboxNoTopText.addEventListener("input", () => {
   $pTopText.remove()
 })
 
-
 /* 💥💥💥💥💥💥💥💥💥 WRITE BOTTOM TEXT 💥💥💥💥💥💥💥💥💥 */
 $textareaBottomText.addEventListener("input", () => {
   $pBottomText.innerText = $textareaBottomText.value
 
 })
 
-
 /* 💥💥💥💥💥💥💥💥💥 NO BOTTOM TEXT 💥💥💥💥💥💥💥💥💥💥 */
 $inputCheckboxNoBottomText.addEventListener("input", () => {
   $pBottomText.remove()
-})  
-   
+})
 
-/* ✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨ */ 
-
+/* 💥💥💥💥💥💥💥💥💥 SELECT FONT 💥💥💥💥💥💥💥💥💥💥 */
+$selectFontFamily.addEventListener("input", () => {
+  $pTopText.style.fontFamily = $selectFontFamily.value
+})
+$selectFontFamily.addEventListener("input", () => {
+  $pBottomText.style.fontFamily = $selectFontFamily.value
+})
